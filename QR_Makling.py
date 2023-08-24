e1bf55f51cdf8054336031e805caa5e11d9d56c8
Prashant Kumar Parth <137444798+prashantkparth@users.noreply.github.com>
2023-08-25 03:55:14 +0530

    documentation QR_Makling.py

--- QR_Makling.py
+++ QR_Makling.py
@@ -1,5 +1,28 @@
+#Importing the 'qrcode' library
 import qrcode as qr
+
+#Here, the code imports the 'qrcode' library and renames it to 'qr' for convenience.
+#This library allows you to create QR codes easily.
+
+
+
+#Generating the QR code:
 img = qr.make("https://github.com/prashantkparth")
+
+#In this line, the 'qr.make()' function is used to generate a QR code.
+#The function takes a single argument, which is the data you want to encode into the QR code.
+#In this case, it's a URL: "https://github.com/prashantkparth".
+#The function returns a QR code object, which is stored in the variable 'img'.
+
+
+#Saving the QR code as an image:
 img.save("GitHub_Prashant.png")
 
+#After generating the QR code, the code saves it as an image file named "GitHub_Prashant.png".
+#The 'save()' method is called on the QR code object ('img'),and it specifies the filename to save the image as.
 
+#The image will be saved in the same directory where the Python script is executed.
+
+
+'''To summarize, this code generates a QR code containing the URL "https://github.com/prashantkparth" and saves it as an image file named "GitHub_Prashant.png".
+When scanned, the QR code would direct a QR code scanner to open the specified URL.'''
